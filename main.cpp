5d73484fdae861ba9432ac84d4b1a5d61b4d48b9
Arvind <arvind@servelots.com>
2011-02-15 01:19:57 +0530

    Password is now not echoed on the terminal, QString implemented for username and password.  Username has to be just the ID, e.g john.doe

--- main.cpp
+++ main.cpp
@@ -23,12 +23,14 @@
 
 
 #include<cstdio>
+#include<iostream>
 #include<cstdlib>
 #include <QtCore/QCoreApplication>
 #include "xmppClient.h"
 #include "QXmppLogger.h"
-
-using namespace::std;
+//#include <QString.h>
+#include<termios.h>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -36,16 +38,27 @@ int main(int argc, char *argv[])
     
     QXmppLogger::getLogger()->setLoggingType(QXmppLogger::FileLogging);
 
-    xmppClient client;
-   char uid[30], pwd[30];
+   xmppClient client;
+   QString username, password;
+   std::string uid,pwd;
    /*getting uid and pwd*/
-   fprintf(stdout,"\nEnter your gmail username:(with @gmail.com):");
-   fscanf(stdin,"%s",uid);
-   fprintf(stdout,"\nEnter your password:");
-   fscanf(stdin,"%s",pwd);
-
+   cout<<"\nEnter your gmail username:";
+   std::getline(cin,uid);
+   uid.append("@gmail.com");
+   username = QString::fromStdString(uid);
+   cout<<qPrintable(username);
+   cout<<endl<<"\nEnter your password:";
+   termios oldt;                                          //This has been added so that password does not echo, when typed.
+   tcgetattr(STDIN_FILENO, &oldt);
+   termios newt = oldt;
+   newt.c_lflag &= ~ECHO;
+   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+   std::getline(cin,pwd);
+   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);              //Returns back the old settings of the terminal/stdout
     /*connecting to server*/
-    client.connectToServer(uid,pwd);
+   
+   password = QString::fromStdString(pwd);
+   client.connectToServer(username,password);
 
 
     return a.exec();
